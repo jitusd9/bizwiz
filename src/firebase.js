5b2be6d0f63bdad7bbf379eeecad896ce550ca1c
jitusd9 <jitusinghonmail@gmail.com>
2022-03-31 18:29:32 +0530

    added env file

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWith
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuyOwsLttyCQ2-4csnXQ2FhEPgCLabzWk",
-  authDomain: "bizwiz-3d98c.firebaseapp.com",
-  projectId: "bizwiz-3d98c",
-  storageBucket: "bizwiz-3d98c.appspot.com",
-  messagingSenderId: "606677032471",
-  appId: "1:606677032471:web:5955719e1ab5dc3e0bc652",
-  measurementId: "G-2CDND1SCRZ"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  };
 
 
 // Initialize Firebase
